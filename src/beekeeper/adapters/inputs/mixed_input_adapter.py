e7249560dd422b067acc6c3c4cb8737e351676b7
Xiddoc <xiddoc@xiddoc.pc>
2025-04-06 08:22:20 +0300

    Create mixed input adapter"

---
+++ src/beekeeper/adapters/inputs/mixed_input_adapter.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+from beekeeper.adapters.inputs.allocation_input_adapter import AllocationInputAdapter
+from beekeeper.adapters.inputs.entity_input_adapter import EntityInputAdapter
+from beekeeper.adapters.inputs.input_adapter import InputAdapter
+
+
+@dataclass
+class MixedInputAdapter(InputAdapter):
+    entity_adapter: EntityInputAdapter
+    allocation_adapter: AllocationInputAdapter
+
+    def get_entities(self):
+        return self.entity_adapter.get_entities()
+
+    def get_allocations(self):
+        return self.allocation_adapter.get_allocations()
