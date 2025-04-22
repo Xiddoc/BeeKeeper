0cab43bd8afd4ea4d8f1a19430575bf7f3103792
basil <hajyehiabasil@gmail.com>
2025-04-22 17:42:13 +0300

    Refactor excel adapter structure

---
+++ examples/mcdonalds/adapters/excel/__init__.py
@@ -0,0 +1,9 @@
+from .excel_allocation_input_adapter import ExcelAllocationInputAdapter
+from .excel_entity_input_adapter import ExcelEntityInputAdapter
+
+
+
+__all__ = [
+    "ExcelAllocationInputAdapter",
+    "ExcelEntityInputAdapter",
+]
