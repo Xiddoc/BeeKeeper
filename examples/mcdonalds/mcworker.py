963a4a861c4b2c6389db5fd4ddc3ef10dfa3a9a2
Xiddoc <xiddoc@xiddoc.pc>
2025-06-01 16:19:10 +0300

    Remove references to dataclass; all classes are now BaseModels

--- examples/mcdonalds/mcworker.py
+++ examples/mcdonalds/mcworker.py
@@ -1,8 +1,5 @@
-from dataclasses import dataclass
-
 from beekeeper import Entity
 
 
-@dataclass
 class McWorker(Entity):
     name: str
--- src/beekeeper/allocations/allocation_request.py
+++ src/beekeeper/allocations/allocation_request.py
@@ -1,6 +1,7 @@
 from collections.abc import Iterable
-from dataclasses import dataclass
 from typing import Optional
+
+from pydantic import BaseModel
 
 from beekeeper.allocations.allocation_type import AllocationType
 from beekeeper.entities.entity import Entity
@@ -8,8 +9,7 @@ from beekeeper.entities.entity_properties import Exemption, Location, Rank
 from beekeeper.time_constructs.date_range import DateRange
 
 
-@dataclass
-class AllocationRequest:
+class AllocationRequest(BaseModel):
     allocation_type: AllocationType
     date_range: DateRange
     location: Location
